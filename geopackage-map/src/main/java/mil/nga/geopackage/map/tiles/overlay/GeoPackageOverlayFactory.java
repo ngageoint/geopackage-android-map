2774217e82fd54673f33d082b5ece98060b8e3aa
Brian Osborn <bosborn@users.noreply.github.com>
2018-04-17 15:04:31 -0600

    overlay factory helper methods for creating composite overlays and linked feature overlays

--- geopackage-map/src/main/java/mil/nga/geopackage/map/tiles/overlay/GeoPackageOverlayFactory.java
+++ geopackage-map/src/main/java/mil/nga/geopackage/map/tiles/overlay/GeoPackageOverlayFactory.java
@@ -3,6 +3,12 @@ package mil.nga.geopackage.map.tiles.overlay;
 import com.google.android.gms.maps.model.Tile;
 import com.google.android.gms.maps.model.TileProvider;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import mil.nga.geopackage.GeoPackage;
+import mil.nga.geopackage.extension.link.FeatureTileTableLinker;
 import mil.nga.geopackage.extension.scale.TileScaling;
 import mil.nga.geopackage.tiles.retriever.GeoPackageTile;
 import mil.nga.geopackage.tiles.user.TileDao;
@@ -67,6 +73,79 @@ public class GeoPackageOverlayFactory {
     public static BoundedOverlay getBoundedOverlay(TileDao tileDao, TileScaling scaling) {
         return new GeoPackageOverlay(tileDao, scaling);
     }
+
+    /**
+     * Create a composite overlay by first adding a tile overlay for the tile DAO followed by the provided overlay
+     *
+     * @param tileDao tile dao
+     * @param overlay bounded overlay
+     * @return composite overlay
+     */
+    public static CompositeOverlay getCompositeOverlay(TileDao tileDao, BoundedOverlay overlay) {
+        List<TileDao> tileDaos = new ArrayList<>();
+        tileDaos.add(tileDao);
+        return getCompositeOverlay(tileDaos, overlay);
+    }
+
+    /**
+     * Create a composite overlay by first adding tile overlays for the tile DAOs followed by the provided overlay
+     *
+     * @param tileDaos collection of tile daos
+     * @param overlay  bounded overlay
+     * @return composite overlay
+     */
+    public static CompositeOverlay getCompositeOverlay(Collection<TileDao> tileDaos, BoundedOverlay overlay) {
+
+        CompositeOverlay compositeOverlay = getCompositeOverlay(tileDaos);
+
+        compositeOverlay.addOverlay(overlay);
+
+        return compositeOverlay;
+    }
+
+    /**
+     * Create a composite overlay by adding tile overlays for the tile DAOs
+     *
+     * @param tileDaos collection of tile daos
+     * @return composite overlay
+     */
+    public static CompositeOverlay getCompositeOverlay(Collection<TileDao> tileDaos) {
+
+        CompositeOverlay compositeOverlay = new CompositeOverlay();
+
+        for (TileDao tileDao : tileDaos) {
+            BoundedOverlay boundedOverlay = GeoPackageOverlayFactory.getBoundedOverlay(tileDao);
+            compositeOverlay.addOverlay(boundedOverlay);
+        }
+
+        return compositeOverlay;
+    }
+
+    /**
+     * Create a composite overlay linking the feature overly with
+     *
+     * @param featureOverlay feature overlay
+     * @param geoPackage     GeoPackage
+     * @return linked bounded overlay
+     */
+    public static BoundedOverlay getLinkedFeatureOverlay(FeatureOverlay featureOverlay, GeoPackage geoPackage) {
+
+        BoundedOverlay overlay;
+
+        // Get the linked tile daos
+        FeatureTileTableLinker linker = new FeatureTileTableLinker(geoPackage);
+        List<TileDao> tileDaos = linker.getTileDaosForFeatureTable(featureOverlay.getFeatureTiles().getFeatureDao().getTableName());
+
+        if (!tileDaos.isEmpty()) {
+            // Create a composite overlay to search for existing tiles before drawing from features
+            overlay = getCompositeOverlay(tileDaos, featureOverlay);
+        } else {
+            overlay = featureOverlay;
+        }
+
+        return overlay;
+    }
+
 
     /**
      * Get a map tile from the GeoPackage tile
