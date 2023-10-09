224dc85cc191cc3f26b0e1d016e84d89e9e81ecc
Brian Osborn <bosborn@gmail.com>
2023-10-09 13:06:26 -0600

    Javadoc comments

--- geopackage-map/src/main/java/mil/nga/geopackage/map/geom/GoogleMapShapeType.java
+++ geopackage-map/src/main/java/mil/nga/geopackage/map/geom/GoogleMapShapeType.java
@@ -7,40 +7,94 @@ package mil.nga.geopackage.map.geom;
  */
 public enum GoogleMapShapeType {
 
+	/**
+	 * {@link com.google.android.gms.maps.model.LatLng}
+	 */
 	LAT_LNG,
 
+	/**
+	 * {@link com.google.android.gms.maps.model.MarkerOptions}
+	 */
 	MARKER_OPTIONS,
 
+	/**
+	 * {@link com.google.android.gms.maps.model.PolylineOptions}
+	 */
 	POLYLINE_OPTIONS,
 
+	/**
+	 * {@link com.google.android.gms.maps.model.PolygonOptions}
+	 */
 	POLYGON_OPTIONS,
 
+	/**
+	 * {@link MultiLatLng}
+	 */
 	MULTI_LAT_LNG,
 
+	/**
+	 * {@link MultiPolylineOptions}
+	 */
 	MULTI_POLYLINE_OPTIONS,
 
+	/**
+	 * {@link MultiPolygonOptions}
+	 */
 	MULTI_POLYGON_OPTIONS,
 
+	/**
+	 * {@link com.google.android.gms.maps.model.Marker}
+	 */
 	MARKER,
 
+	/**
+	 * {@link com.google.android.gms.maps.model.Polyline}
+	 */
 	POLYLINE,
 
+	/**
+	 * {@link com.google.android.gms.maps.model.Polygon}
+	 */
 	POLYGON,
 
+	/**
+	 * {@link MultiMarker}
+	 */
 	MULTI_MARKER,
 
+	/**
+	 * {@link MultiPolyline}
+	 */
 	MULTI_POLYLINE,
 
+	/**
+	 * {@link MultiPolygon}
+	 */
 	MULTI_POLYGON,
 
+	/**
+	 * {@link PolylineMarkers}
+	 */
 	POLYLINE_MARKERS,
 
+	/**
+	 * {@link PolygonMarkers}
+	 */
 	POLYGON_MARKERS,
 
+	/**
+	 * {@link MultiPolylineMarkers}
+	 */
 	MULTI_POLYLINE_MARKERS,
 
+	/**
+	 * {@link MultiPolygonMarkers}
+	 */
 	MULTI_POLYGON_MARKERS,
 
+	/**
+	 * Collection of shapes
+	 */
 	COLLECTION;
 
 }
